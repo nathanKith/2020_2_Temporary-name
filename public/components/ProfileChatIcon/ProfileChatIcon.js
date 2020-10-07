a09d4893d52e9c97555ff22d8d98fb19003b8d92
nathanKith <ntolparov@gmail.com>
2020-10-07 03:27:42 +0300

    add profile

---
+++ public/components/ProfileChatIcon/ProfileChatIcon.js
@@ -0,0 +1,44 @@
+export default class ProfileChatIcon {
+    #parent
+
+    constructor(parent) {
+        this.#parent = parent;
+    }
+
+    render() {
+        const div = this.#createDiv('profile-chat-icons');
+
+        const profile = this.#createDiv('profile-icon');
+        const chats = this.#createDiv('chats-icon');
+
+        const profileButton = document.createElement('button');
+        profileButton.classList.add('profile-icon-button')
+        profileButton.type = 'button';
+        profileButton.innerHTML = `<img src="./../../img/profile.svg">`;
+
+        const chatsButton = document.createElement('button');
+        chatsButton.classList.add('chats-icon-button')
+        chatsButton.type = 'button';
+        chatsButton.innerHTML = `<img src="./../../img/chats.svg">`;
+
+        profile.appendChild(profileButton);
+        chats.appendChild(chatsButton);
+
+        div.appendChild(profile);
+        div.appendChild(chats);
+
+        this.#parent.appendChild(div);
+
+        return {
+            profileButton: profileButton,
+            chatsButton: chatsButton,
+        };
+    }
+
+    #createDiv = (className) => {
+        const div = document.createElement('div');
+        div.classList.add(className);
+
+        return div;
+    }
+}

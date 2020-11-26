7e4c9ec036966fae1cddfe6f0bd02b282c7aeccb
Amaterasy17 <sheb70017@gmail.com>
2020-11-27 01:32:11 +0300

    doing of albums

--- public/components/Album/Album.js
+++ public/components/Album/Album.js
@@ -1,4 +1,7 @@
 import AlbumImg from './AlbumImg.hbs'
+import AlbumPreview from './AlbumPreview.hbs'
+import AlbumButtons from './AlbumButtons.hbs'
+import readImage from '../../modules/previewAvatar'
 
 export class Album {
     #parent
@@ -15,15 +18,15 @@ export class Album {
         albumSection.classList.add('album-section');
         this.#parent.appendChild(albumSection);
 
+
+        albumSection.insertAdjacentHTML('beforeend', AlbumPreview());
+
         this.#listImages.forEach( (image) => {
             albumSection.insertAdjacentHTML('beforeend', AlbumImg({
                 photo: image,
             }));
         } )
 
-        albumSection.insertAdjacentHTML('beforeend', AlbumImg({
-            photo: './plus.svg',
-        }));
     }
 
     newPhoto() {

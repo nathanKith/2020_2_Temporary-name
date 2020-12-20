dcbd474635d058c1d75272542d514b485c20d499
Andrey <andmavrin@yandex.ru>
2020-12-20 00:23:23 +0000

    finished swipes on mobile

--- public/views/FeedMobileView.js
+++ public/views/FeedMobileView.js
@@ -65,6 +65,11 @@ export class FeedMobileView extends BaseView {
         feed.data = this._context['feed'];
         feed.render();
 
+        const profilePerson = document.getElementsByClassName('profile-person')[0];
+        const swipes = new Swipes(profilePerson);
+        swipes.data = this._context['feed'];
+        swipes.control();
+
         const newInformationLogo = document.getElementById('information-logo');
         newInformationLogo.addEventListener('click', this.#getCommentsListener.bind(this));
     }
